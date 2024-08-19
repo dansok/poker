bd1ee7365d0f65143c182560a725400f29d521cc
Boris <test2001@sharklasers.com>
2024-08-19 19:36:16 -0400

    .

--- random_env.py
+++ random_env.py
@@ -7,38 +7,43 @@ class RandomEnv:
         self.players = None
         self.community_cards = None
         self.pot = None
-        self.current_raise = None
-        self.num_folds = None
         self.reset()
 
     def play_round(self):
+        round_raise_amount = 0
+        pot = 0
         for player in self.players:
-            action = player.act(current_raise=self.current_raise)
-            self.pot += action[1]
+            action = player.act(round_raise_amount=round_raise_amount)
+            pot += action[1]
             if action[0] == ACTION.RAISE:
-                self.current_raise = action[1]
+                round_raise_amount += action[1]
 
     def play(self):
-        self.play_round()
-        self.community_cards.append(self.deck.draw())
-        self.play_round()
-        self.community_cards.append(self.deck.draw())
-        self.play_round()
+        for _ in range(3):
+            self.community_cards.append(self.deck.draw())
+            self.play_round()
+            if self.check_game_finished():
+                break
 
         winners = []
         for player in self.players:
-            rank = player.hand.rank_hand(self.community_cards)
-            if len(winners) == 0 or winners[0][1][0] < rank[0]:
-                winners = [[player, rank]]
-            elif winners[0][1][0] == rank[0]:
-                winners.append([player, rank])
+            if player.last_action != ACTION.FOLD:
+                rank = player.hand.rank_hand(self.community_cards)
+                if len(winners) == 0 or winners[0][1][0] < rank[0]:
+                    winners = [[player, rank]]
+                elif winners[0][1][0] == rank[0]:
+                    winners.append([player, rank])
         for winner in winners:
             winner[0].money += self.pot / len(winners)
+
+    def check_game_finished(self):
+        for player in self.players:
+            if player.last_action != ACTION.CHECK and player.last_action != ACTION.FOLD:
+                return False
+        return True
 
     def reset(self):
         self.deck = Deck()
         self.players = [RandomPlayer(i, [self.deck.draw(), self.deck.draw()]) for i in range(5)]
-        self.community_cards = [self.deck.draw(), self.deck.draw(), self.deck.draw()]
+        self.community_cards = [self.deck.draw(), self.deck.draw()] # third card will be appended at the beginning of the first round
         self.pot = 0
-        self.current_raise = 0
-        self.num_folds = len(self.players)
--- random_player.py
+++ random_player.py
@@ -23,25 +23,31 @@ class RandomPlayer:
         self.player_id = player_id  # Unique identifier for the player
         self.hand = Hand(hand)  # Initial hand dealt to the player (two cards)
         self.money = 1_000
+        self.last_action = None
 
-    def act(self, current_raise = 0):
-        if self.money == 0:
-            return ACTION.FOLD, 0, 0
-
-        action = ACTION.select_random()
-        if action == ACTION.CALL and self.money < current_raise:
-            action = ACTION.CHECK
-
+    def act(self, round_raise_amount = 0):
+        action = self.select_action(round_raise_amount)
+        add_to_pot = 0
         if action == ACTION.RAISE:
-            amount = random.randint(1, self.money)
-            self.money -= amount
-            return ACTION.RAISE, amount, self.money
+            add_to_pot = round_raise_amount + random.randint(1, self.money - round_raise_amount)
         elif action == ACTION.CALL:
-            amount = current_raise
-            self.money -= amount
-            return ACTION.RAISE, amount, self.money
-        elif action == ACTION.FOLD:
-            return ACTION.FOLD, 0, self.money
+            add_to_pot = round_raise_amount
+        self.money -= add_to_pot
+        self.last_action = action
+        return action, add_to_pot
 
-        return action, 0, self.money
+    def select_action(self, current_raise_amount):
+        if self.last_action == ACTION.FOLD:
+            return ACTION.FOLD
 
+        if self.money == 0 or self.money < current_raise_amount:
+            action = ACTION.FOLD
+        else:
+            action = ACTION.select_random()
+
+        if action == ACTION.CALL and current_raise_amount == 0:
+            action = ACTION.CHECK
+        if action == ACTION.RAISE and current_raise_amount == self.money:
+            action = ACTION.CALL
+
+        return action
