89c952290904bde7da3ae587883f2d28544d1c53
Dan Sokolsky <dan.sokolsky@gmail.com>
2024-09-24 17:41:25 -0400

    reformatting main.py

--- main.py
+++ main.py
@@ -22,32 +22,32 @@ from card import Card
 
 
 def main():
-    out_file = open('play_results.csv', 'w')
-    out_file.write(
-        'my_card_1, my_card_2, community_card_1, community_card_2,community_card_3,community_card_4,community_card_5,weighted_output')
-    for _ in range(10000):
-        deck = Deck()
+    with open('play_results.csv', 'w') as out_file:
+        out_file.write(
+            'my_card_1, my_card_2, community_card_1, community_card_2,community_card_3,community_card_4,'
+            'community_card_5,weighted_output')
+        for _ in range(10_000):
+            deck = Deck()
 
-        hands = [Hand(cards=[deck.draw(), deck.draw()]) for _ in range(6)]
-        community_cards = [deck.draw(), deck.draw(), deck.draw(), deck.draw(), deck.draw()]
+            hands = [Hand(cards=[deck.draw(), deck.draw()]) for _ in range(6)]
+            community_cards = [deck.draw(), deck.draw(), deck.draw(), deck.draw(), deck.draw()]
 
-        ranks = OrderedDict({hand: hand.rank_hand(community_cards=community_cards) for hand in hands})
-        ranks = sorted(ranks.items(), key=lambda item: item[1])
+            ranks = OrderedDict({hand: hand.rank_hand(community_cards=community_cards) for hand in hands})
+            ranks = sorted(ranks.items(), key=lambda item: item[1])
 
-        print(f"ranks == {ranks}")
-        my_hand = hands[0]
-        best_rank = ranks[len(ranks) - 1]
-        weighted_score = 0
-        if best_rank[0] == my_hand:
-            score = best_rank[1][0]
-            number_of_winners = 0
-            for rank in ranks:
-                if score == rank[1][0]:
-                    number_of_winners += 1
-            weighted_score = 1 / number_of_winners
-        all_cards = my_hand.cards + community_cards
-        out_file.write('\n' + ','.join(str(Card.card_to_index(card)) for card in all_cards) + f',{weighted_score}')
-    out_file.close()
+            print(f"ranks == {ranks}")
+            my_hand = hands[0]
+            best_rank = ranks[len(ranks) - 1]
+            weighted_score = 0
+            if best_rank[0] == my_hand:
+                score = best_rank[1][0]
+                number_of_winners = 0
+                for rank in ranks:
+                    if score == rank[1][0]:
+                        number_of_winners += 1
+                weighted_score = 1 / number_of_winners
+            all_cards = my_hand.cards + community_cards
+            out_file.write('\n' + ','.join(str(Card.card_to_index(card)) for card in all_cards) + f',{weighted_score}')
 
 
 if __name__ == "__main__":
